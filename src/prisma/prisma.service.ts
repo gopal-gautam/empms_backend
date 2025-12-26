bb82d899af5b5e87a57f80ab835430dab3836e7a
gopalg <gopal@gopal>
2025-12-26 20:10:59 +1100

    Intial commit

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,28 @@
 import 'dotenv/config';
-import { Injectable } from "@nestjs/common";
+import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 
-
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
     constructor() {
-        const url = process.env.DATABASE_URL ?? 'file:./dev.db';
-        const adapter = new PrismaBetterSqlite3({ url });
-        super({ adapter });
+        const databaseProvider = process.env.DATABASE_PROVIDER || 'sqlite';
+
+        if (databaseProvider === 'sqlite') {
+            const url = process.env.DATABASE_URL ?? 'file:./dev.db';
+            const adapter = new PrismaBetterSqlite3({ url });
+            super({ adapter } as any);
+        } else {
+            // @ts-ignore - PostgreSQL doesn't need adapter
+            super();
+        }
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async onModuleDestroy() {
+        await this.$disconnect();
     }
 }
