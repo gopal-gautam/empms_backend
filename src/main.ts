1ac6ce7936fdf05e742c39394d0b6efa03f93d26
gopalg <gopal@gopal>
2025-12-27 09:34:06 +1100

    allow cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://hremployeems.vercel.app/'], // add your frontend domains here
+    origin: ['http://localhost:5173', 'https://hremployeems.vercel.app'], // add your frontend domains here
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // only if you use cookies/auth
