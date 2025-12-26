80d5991021fdeda261d972d41e898c4a6602a836
gopalg <gopal@gopal>
2025-12-26 23:59:39 +1100

    use tsc to build at prod

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173'], // add your frontend domains here
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // only if you use cookies/auth
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
